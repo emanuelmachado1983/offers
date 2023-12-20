4d4f4e3567348cd170d4b3af6dacbbdce5477064
unknown <emanuelmachado77@gmail.com>
2023-12-19 23:39:06 -0300

    added some comments

--- src/main/java/com/emanuel/offers/application/service/OfferService.java
+++ src/main/java/com/emanuel/offers/application/service/OfferService.java
@@ -46,31 +46,42 @@ public class OfferService {
 		if (offers.isEmpty()) {
 			throw new EmptyOffersSelectedException();
 		}
+		
+		//First: Validate the user
 		try {
 			clientRepository.validateUser(userCode);
 		} catch (UserNotValidated userNotValidated) {
 			emailRepository.sendEmail(userCode, userNotValidated);
 			throw userNotValidated;
 		}  catch (Exception e) {
+			//if there is an error in the validation of the user I save it in the database, but with the flags in false, and the flag of continue trying in true.
+			//the job will try to repeat this method every 30 minutes for two hours.
 			sendOffersToDatabase(userCode, paymentMethod, offers, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE);
 			emailRepository.sendEmail(userCode, e);
 			throw e;
 		}
 		
+		//Then: Validate the payment method
 		try {
 			paymentsRepository.validatePaymentMethod(userCode, paymentMethod);
 		} catch (PaymentMethodInvalid paymentMethodInvalid) {
 			emailRepository.sendEmail(userCode, paymentMethodInvalid);
 			throw paymentMethodInvalid;
 		}  catch (Exception e) {
+			//if there is an error in the validation I save it in the database, but with the flag in false, and with the flag of continue trying in true.
+			//the job will try to repeat this method every 30 minutes for two hours.
 			sendOffersToDatabase(userCode, paymentMethod, offers, Boolean.TRUE, Boolean.FALSE, Boolean.TRUE);
 			emailRepository.sendEmail(userCode, e);
 			throw e;
 		}
+		
+		//Then send the offers to the database
 		sendOffersToDatabase(userCode, paymentMethod, offers, Boolean.TRUE, Boolean.TRUE, Boolean.FALSE);
 
+		//Then send the notification to the Platform Decision
 		decisionPlatformRepository.notification(userCode, offers, paymentMethod);
 		
+		//Then send the notificacion to the banking entities.
 		bankingEntitiesRepository.notification(userCode, offers, paymentMethod);
 	}
 	
